c4e3d59622201ee2e11c89b4fdf7bd24a6464e2d
Francisco Marin <francisco.marin@ceiba.com.co>
2020-03-26 12:57:34 -0500

    mysql implementation

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,9 +2,19 @@ package users
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/fmarinCeiba/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/fmarinCeiba/bookstore_users-api/utils/date_utils"
 	"github.com/fmarinCeiba/bookstore_users-api/utils/errors"
+	"github.com/go-sql-driver/mysql"
+)
+
+const (
+	indexUniqueEmail = "email_UNIQUE"
+	errorNoRows      = "no rows in result set"
+	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?, ?, ?, ?);"
+	queryGetAnUser   = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = ?"
 )
 
 var (
@@ -12,29 +22,73 @@ var (
 )
 
 func (u *User) Get() *errors.RestErr {
-	result := usersDB[u.Id]
-	if result == nil {
-		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", u.Id))
+	// if err := users_db.Client.Ping(); err != nil {
+	// 	panic(err)
+	// }
+	stmt, err := users_db.Client.Prepare(queryGetAnUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	u.Id = result.Id
-	u.FirstName = result.FirstName
-	u.LastName = result.LastName
-	u.Email = result.Email
-	u.DateCreated = result.DateCreated
+	defer stmt.Close()
+	iResult := stmt.QueryRow(u.Id)
+	if err := iResult.Scan(&u.Id, &u.FirstName, &u.LastName, &u.Email, &u.DateCreated); err != nil {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError(fmt.Sprintf("user %d not found", u.Id))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to get user %d: %s", u.Id, err.Error()))
+	}
+	// LOCAL INSTANT DB
+	// result := usersDB[u.Id]
+	// if result == nil {
+	// 	return errors.NewNotFoundError(fmt.Sprintf("user %d not found", u.Id))
+	// }
+
+	// u.Id = result.Id
+	// u.FirstName = result.FirstName
+	// u.LastName = result.LastName
+	// u.Email = result.Email
+	// u.DateCreated = result.DateCreated
 	return nil
 }
 
 func (u *User) Save() *errors.RestErr {
-	current := usersDB[u.Id]
-	if current != nil {
-		if current.Email == u.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("user %s already registered", u.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", u.Id))
+	// if err := users_db.Client.Ping(); err != nil {
+	// 	panic(err)
+	// }
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	u.DateCreated = date_utils.GetNowString()
+	iResult, err := stmt.Exec(u.FirstName, u.LastName, u.Email, u.DateCreated)
+	if err != nil {
+		sqlErr, ok := err.(*mysql.MySQLError)
+		if !ok {
+			return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+		}
+		fmt.Println(sqlErr)
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", u.Email))
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+	uId, err := iResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+	u.Id = uId
 
-	usersDB[u.Id] = u
+	// LOCAL INSTANT DB
+	// current := usersDB[u.Id]
+	// if current != nil {
+	// 	if current.Email == u.Email {
+	// 		return errors.NewBadRequestError(fmt.Sprintf("user %s already registered", u.Email))
+	// 	}
+	// 	return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", u.Id))
+	// }
+	// usersDB[u.Id] = u
+
 	return nil
 }
---
+++ utils/mysql_utils/mysql_utils.go
@@ -0,0 +1,29 @@
+package mysql_utils
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fmarinCeiba/bookstore_users-api/utils/errors"
+	"github.com/go-sql-driver/mysql"
+)
+
+const (
+	errorNoRows = "no rows in result set"
+)
+
+func ParseError(err error) *errors.RestErr {
+	sqlErr, ok := err.(*mysql.MySQLError)
+	if !ok {
+		if strings.Contains(err.Error(), errorNoRows) {
+			return errors.NewNotFoundError("no record matching given id")
+		}
+		return errors.NewInternalServerError("error parsing database response")
+	}
+	fmt.Println(sqlErr)
+	switch sqlErr.Number {
+	case 1062:
+		return errors.NewBadRequestError("duplicated key data")
+	}
+	return errors.NewInternalServerError("error processing request")
+}
